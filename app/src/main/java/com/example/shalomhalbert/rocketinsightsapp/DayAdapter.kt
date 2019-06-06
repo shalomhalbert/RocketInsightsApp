cb326131a9c7ac06ae14a9c8ee427800b2954848
Shalom Halbert <shalomhalbert@gmail.com>
2019-06-06 17:15:53 -0400

    Implemented Adapter with Data binding

--- app/src/main/java/com/example/shalomhalbert/rocketinsightsapp/DayAdapter.kt
+++ app/src/main/java/com/example/shalomhalbert/rocketinsightsapp/DayAdapter.kt
@@ -1,25 +1,22 @@
-package com.example.shalomhalbert.rocketinsightsapp.shared
+package com.example.shalomhalbert.rocketinsightsapp
 
 import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import com.example.shalomhalbert.rocketinsightsapp.R
-import kotlinx.android.synthetic.main.rv_day_list_item.view.*
+import com.example.shalomhalbert.rocketinsightsapp.databinding.RvDayListItemBinding
 
-/**
- * [RecyclerView.Adapter] for [HomeFragment]'s RecyclerView
- */
-//Todo: Cleanup, and create ViewModel parameter version to ask audience for opinions
 class DayAdapter : RecyclerView.Adapter<DayAdapter.DayListViewHolder>() {
     private val dates = mutableListOf<Date>()
 
-    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): DayListViewHolder =
-            DayListViewHolder(LayoutInflater.from(p0.context).inflate(R.layout.rv_day_list_item, p0, false))
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DayListViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = RvDayListItemBinding.inflate(inflater, parent, false)
+        return DayListViewHolder(binding)
+    }
 
-    override fun onBindViewHolder(p0: DayListViewHolder, p1: Int) {
-        p0.dateTextView.text = dates!![p1].date
+    override fun onBindViewHolder(holder: DayListViewHolder, position: Int) {
+        val date = dates[position].date
+        holder.binding.date = date
     }
 
     override fun getItemCount(): Int = dates!!.size
@@ -29,10 +26,6 @@ class DayAdapter : RecyclerView.Adapter<DayAdapter.DayListViewHolder>() {
         notifyDataSetChanged()
     }
 
-    /**
-     * [RecyclerView.ViewHolder] Class for [DayAdapter]
-     */
-    class DayListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val dateTextView: TextView = itemView.dateTextView
-    }
+    class DayListViewHolder(val binding: RvDayListItemBinding) :
+        RecyclerView.ViewHolder(binding.root)
 }
