84f0143386917d2f5c7aa0eaa522acd050b521ca
Shalom Halbert <shalomhalbert@gmail.com>
2019-06-07 12:11:17 -0400

    Rm'd Log references and add comment explaining why

--- app/src/main/java/com/example/shalomhalbert/rocketinsightsapp/model/Repository.kt
+++ app/src/main/java/com/example/shalomhalbert/rocketinsightsapp/model/Repository.kt
@@ -5,19 +5,17 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import retrofit2.HttpException
 
-class Repository (private val service: Service) {
+class Repository(private val service: Service) {
     private val TAG: String = Repository::class.java.simpleName
 
+    //Removed Log because it causes problems that Timber would resolve, and I don't care to implement Timber
     suspend fun getDates(): List<Date> =
-         withContext(Dispatchers.IO) {
+        withContext(Dispatchers.IO) {
             try {
                 service.dates().await()
             } catch (e: HttpException) {
-                Log.e(TAG, "HttpException code: ${e.code()}")
                 emptyList<Date>()
             } catch (e: Throwable) {
-                Log.e(TAG, "dates() failed-- " +
-                        "Cause: ${e.cause}, Message: ${e.message}")
                 throw e
             }
         }
